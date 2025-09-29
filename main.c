59b85256efdcf15574cda119aaec722bd14558f1
Alguem5381 <alguem5381@gmail.com>
2025-09-29 09:50:37 -0400

    Definição das structs

--- main.c
+++ main.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX 12
+
+typedef struct Node
+{
+    int index;
+    struct Node *next;
+} Node;
+
+typedef struct
+{
+    char department[MAX];
+    Node *head;
+} Table;
+
+typedef struct
+{
+    char name[MAX];
+    char department[MAX];
+    float salary;
+    char status;
+} Employee;
+
 int main()
 {
-    
+
 
     return 0;
 }
